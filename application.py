6e9646c3efaef274b9fbb33a6eefce45aa33c938
Soyeon-Cha <7103sy@naver.com>
2023-11-15 17:50:27 +0900

    feat: 회원가입 구현

--- application.py
+++ application.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template , request
 from database import DBhandler
+import hashlib
 import sys
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "xzkcljfhe"
 DB = DBhandler()
 
 
@@ -16,16 +18,12 @@ def mypage():
 def login():
     return render_template('login.html')
 
-
-
-
-
-@app.route("/product-add") 
+@app.route('/product-add') 
 def productAdd():
     return render_template('product_add.html')
 
 
-@app.route("/add-product-post", methods=["POST"])
+@app.route('/add-product-post', methods=["POST"])
 def registerproduct():
     data = {
         "product_description": request.form.get("product-description"),
@@ -38,9 +36,32 @@ def registerproduct():
     DB.insert_item(data['product_category'], data)
     return render_template("products_list.html", data=data)
 
+@app.route('/signup1')
+def signup1():
+    return render_template('signup1.html')
 
+@app.route('/signup1_post', methods=["POST"])
+def register_user_1():
+    data = request.form
+    password1 = request.form['password1']
+    pw_hash = hashlib.sha256(password1.encode('utf-8')).hexdigest()
+    if DB.insert_user(data,pw_hash):
+        return render_template("signup2.html")
+    else:
+        return render_template("signup1.html")
 
+@app.route('/signup2')
+def signup2():
+    return render_template('signup2.html')
 
+@app.route('/signup2_post', methods=["POST"])
+def register_user_2():
+    data = {
+        "nickname": request.form.get("nickname"),
+        "profile-img": request.form.get("profile-img")
+    }
+    if DB.insert_user2(data):
+        return render_template("signup3.html")
 
 @app.route("/product-detail")
 def productDetail():
@@ -61,8 +82,6 @@ def reviewDetail():
 @app.route("/reviews-list")
 def reviewList():
     return render_template('reviews_list.html')
-
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- database.py
+++ database.py
@@ -20,6 +20,38 @@ class DBhandler:
         self.db.child("item").child(name).set(item_info)
         print(data)
         return True
+    
+    def insert_user(self, data, password1):
+        user_info = {
+            "name": data['name'],
+            "email": data['email'],
+            "phone": data['phone'],
+            "id": data['id'],
+            "password": password1,
+        }
+        if self.user_duplicate_check(str(data['email'])) and password1!=data['password2']:
+            self.db.child("user").push(user_info)
+            print(data)
+            return True
+        else:
+            return False
 
-
-
+    def user_duplicate_check(self, email_string):
+        users = self.db.child("user").get()
+        print("users###",users.val())
+        if str(users.val()) == "None":
+            return True
+        else:
+            for res in users.each():
+                value = res.val()
+                if value['email'] == email_string:
+                    return False
+                return True
+            
+    def insert_user2(self, data):
+        user_info = {
+            "nickname": data['nickname'],
+            "profile-img": data['profile-img']
+        }
+        self.db.child("user").push(user_info)
+        return True
